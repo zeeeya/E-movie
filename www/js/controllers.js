00b2280af7692a4a9c61d775310b698889073231
zeeeya <zci.alvarez@gmail.com>
2017-02-18 15:41:25 +0800

    hey

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,17 +4,26 @@ angular.module('app.controllers', [])
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $http) {
-	$http.get("https://api.themoviedb.org/3/movie/550?api_key=2fcc2bbc8f7e805847e8708191dd6685").success(function(response){
-		$scope.movie = response;
-		console.log($scope.movie);
-	});
+	
 
 }])
    
-.controller('movieRatingCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('movieRatingCtrl', ['$scope', '$stateParams', '$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $http) {
+$http.get("https://api.themoviedb.org/3/discover/movie?api_key=2fcc2bbc8f7e805847e8708191dd6685&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1").success(function(response){
+$scope.movies = response.results;
+console.log($scope.movies);
+});
+// $http.get('js/data.json').success(function(data){
+// 	$scope.movie = movie.title;
+// 	$scope.movie = movie.releaseDate;
+// 	$scope.movie = movie.releaseCountry;
+// 	$scope.movie = movie.releaseYear;
+// 	$scope.movie = movie.releaseMonth;
+
+// })
 
 
 }])
